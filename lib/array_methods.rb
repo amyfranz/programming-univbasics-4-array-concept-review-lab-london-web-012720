6af07fca13aa91a51657d01cd4a43768d2d5c288
Amy Franz <amy.franz@gmail.com>
2019-12-22 17:08:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,9 +9,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  newArray = array.sort 
+  return newArray[-1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  newArray = array.sort 
+  return newArray[0]
 end
